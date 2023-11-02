ffa1e43e780e336ab0dc701279e9b09320da52cd
Emre Duman <76225090+Emre1Duman@users.noreply.github.com>
2023-11-02 18:13:26 +0000

    Updated next card function

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ BACKGROUND_COLOR = "#B1DDC6"
 # ---------------------------- Data ------------------------------- #
 
 data = pandas.read_csv("data/french_words.csv")
-data_dictionary = data.to_dict()
+to_learn = data.to_dict(orient="records")
 
-def new_french_word():
-    french_word = data_dictionary["French"][random.randint(0,100)]
-    canvas.itemconfig(word, text=f"{french_word}")
+def next_card():
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="French")  
+    canvas.itemconfig(card_word, text=f"{current_card['French']}")
 
 
 
@@ -28,28 +29,22 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image=card_front_img)
-canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"), fill="black")
-word = canvas.create_text(400, 263, text="Word", font=("Ariel", 60, "bold"), fill="black")
+card_title = canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"), fill="black")
+card_word = canvas.create_text(400, 263, text="Word", font=("Ariel", 60, "bold"), fill="black")
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(column=0, row=0, columnspan=2)
 
 
 ##BUTTONS##
 cross_image = PhotoImage(file="images/wrong.png")
-wrong_button = Button(image=cross_image, highlightthickness=0, highlightbackground=BACKGROUND_COLOR, command=new_french_word)
+wrong_button = Button(image=cross_image, highlightthickness=0, highlightbackground=BACKGROUND_COLOR, command=next_card)
 wrong_button.grid(column=0, row=1)
 
 check_image = PhotoImage(file="images/right.png")
-right_button = Button(image=check_image, highlightthickness=0, highlightbackground=BACKGROUND_COLOR, command=new_french_word)
+right_button = Button(image=check_image, highlightthickness=0, highlightbackground=BACKGROUND_COLOR, command=next_card)
 right_button.grid(column=1, row=1)
 
-
-
-
-
-
-
-
+next_card()
 
 
 window.mainloop()
