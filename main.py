ae668903aa3269ebf108f1b41d5c588f5a652478
Emre Duman <76225090+Emre1Duman@users.noreply.github.com>
2023-11-02 19:49:03 +0000

    main

--- main.py
+++ main.py
@@ -6,12 +6,21 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 data = pandas.read_csv("data/french_words.csv")
 to_learn = data.to_dict(orient="records")
+current_card = {}
 
 def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
     current_card = random.choice(to_learn)
-    canvas.itemconfig(card_title, text="French")  
-    canvas.itemconfig(card_word, text=f"{current_card['French']}")
+    canvas.itemconfig(card_title, text="French", fill="black")  
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image=card_front_img)
+    flip_tmer = window.after(3000, flip_card)
 
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_img)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -20,11 +29,14 @@ window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(3000, flip_card)
+
+
 ##CANVAS##
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
 card_back_img = PhotoImage(file="images/card_back.png")
-canvas_image = canvas.create_image(400, 263, image=card_front_img)
+card_background = canvas.create_image(400, 263, image=card_front_img)
 
 card_title = canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"), fill="black")
 card_word = canvas.create_text(400, 263, text="Word", font=("Ariel", 60, "bold"), fill="black")
@@ -43,13 +55,8 @@ right_button.grid(column=1, row=1)
 
 next_card()
 
-# ---------------------------- FLIPPING FLASH CARDS ------------------------------- #
 
-def flip_card():
-    canvas.itemconfig(canvas_image, image=card_back_img)
-    canvas.itemconfig(card_title, text="English", fill="white")
 
-window.after(3000, flip_card)
 
 
 window.mainloop()
