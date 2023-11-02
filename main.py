e87d7d3e9f11b661670badd40a902e44c97d7c14
Emre Duman <76225090+Emre1Duman@users.noreply.github.com>
2023-11-02 18:55:20 +0000

    Started Flip card function

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from tkinter import *
 import pandas, random
 BACKGROUND_COLOR = "#B1DDC6"
 
-
-# ---------------------------- Data ------------------------------- #
+# ---------------------------- NEW FLASH CARDS ------------------------------- #
 
 data = pandas.read_csv("data/french_words.csv")
 to_learn = data.to_dict(orient="records")
@@ -12,10 +11,6 @@ def next_card():
     current_card = random.choice(to_learn)
     canvas.itemconfig(card_title, text="French")  
     canvas.itemconfig(card_word, text=f"{current_card['French']}")
-
-
-
-
 
 
 
@@ -28,7 +23,9 @@ window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 ##CANVAS##
 canvas = Canvas(width=800, height=526)
 card_front_img = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=card_front_img)
+card_back_img = PhotoImage(file="images/card_back.png")
+canvas_image = canvas.create_image(400, 263, image=card_front_img)
+
 card_title = canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"), fill="black")
 card_word = canvas.create_text(400, 263, text="Word", font=("Ariel", 60, "bold"), fill="black")
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
@@ -45,6 +42,14 @@ right_button = Button(image=check_image, highlightthickness=0, highlightbackgrou
 right_button.grid(column=1, row=1)
 
 next_card()
+
+# ---------------------------- FLIPPING FLASH CARDS ------------------------------- #
+
+def flip_card():
+    canvas.itemconfig(canvas_image, image=card_back_img)
+    canvas.itemconfig(card_title, text="English", fill="white")
+
+window.after(3000, flip_card)
 
 
 window.mainloop()
