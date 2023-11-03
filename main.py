64671f3563a9cc883a4418fd3f1e57b626edb035
Emre Duman <76225090+Emre1Duman@users.noreply.github.com>
2023-11-03 22:44:47 +0000

    saving progress to external csv

--- main.py
+++ main.py
@@ -3,9 +3,16 @@ import pandas, random
 BACKGROUND_COLOR = "#B1DDC6"
 
 # ---------------------------- NEW FLASH CARDS ------------------------------- #
+to_learn = {}
 
-data = pandas.read_csv("data/french_words.csv")
-to_learn = data.to_dict(orient="records")
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/french_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
+
 current_card = {}
 
 def next_card():
@@ -22,6 +29,13 @@ def flip_card():
     canvas.itemconfig(card_word, text=current_card["English"], fill="white")
     canvas.itemconfig(card_background, image=card_back_img)
 
+def is_known():
+    to_learn.remove(current_card)
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv")
+
+    next_card()
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -29,7 +43,7 @@ window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-flip_timer = window.after(3000, flip_card)
+flip_timer = window.after(4000, flip_card)
 
 
 ##CANVAS##
@@ -50,7 +64,7 @@ wrong_button = Button(image=cross_image, highlightthickness=0, highlightbackgrou
 wrong_button.grid(column=0, row=1)
 
 check_image = PhotoImage(file="images/right.png")
-right_button = Button(image=check_image, highlightthickness=0, highlightbackground=BACKGROUND_COLOR, command=next_card)
+right_button = Button(image=check_image, highlightthickness=0, highlightbackground=BACKGROUND_COLOR, command=is_known)
 right_button.grid(column=1, row=1)
 
 next_card()
